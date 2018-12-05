44fe69d0f514557c0ed94ad7087f6e176f61a9bc
Дмитрий Багаев <sequensor@mail.ru>
2018-12-05 22:10:54 +0600

    add gulp

--- Practice/Practice4/gulpfile.js
+++ Practice/Practice4/gulpfile.js
@@ -3,13 +3,18 @@
 // Подключение плагинов
 var gulp = require('gulp'),
     clean = require('gulp-clean');
+    copy = require('gulp-contrib-copy');
+    concat = require('gulp-concat');
  
 // Пути для сборки
 var path = {
     build: {},
-    src: {},
+    src: {
+    	copy_html: 'src/templates/index.html'
+    	css_files: 'src/styles/*.css'
+    },
     watch: {},
-    clean: 'build'
+    clean: ['build','src/styles/main.css']
 };
  
 // Очистка папок и файлов
@@ -17,3 +22,15 @@ gulp.task('clean', function() {
     return gulp.src(path.clean, {read: false})
         .pipe(clean());
 });
+
+gulp.task('html:copy', function() {
+    gulp.src(path.src.copy_html)
+        .pipe(copy())
+        .pipe(gulp.dest(path.build));
+});
+
+gulp.task('css:concat', function() {
+    return gulp.src(path.src.styles)
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest(path.build));
+}); 
