b36bc921dd17775f30be1fa046b3d4a6d60f86a5
Дмитрий Багаев <sequensor@mail.ru>
2018-12-20 18:30:00 +0600

    fix gulp, finish workshop

--- Workshops/Workshop4/gulpfile.js
+++ Workshops/Workshop4/gulpfile.js
@@ -5,41 +5,36 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     copy = require('gulp-contrib-copy'),
     concat = require('gulp-concat'),
-    browserSync = require('browser-sync').create(),
-    reload = browserSync.reload;
+    handlebars = require('gulp-compile-handlebars'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync').create();
 
  
 // Пути для сборки
 var path = {
+    css: './src/**/*.css',
+    html: {
+     pages: "./src/pages/**/*.hbs",
+     components: "./src/components/**/*.hbs",
+     componentsPath: "./src/components/"
+    },
+    images: "./src/**/images/*",
     build: {
+        root: './build/**',
     	html: 'build/',
-    	css: 'build/styles/'
+    	css: 'build/styles/',
+    	images: './build/images/'
     },
     src: {
     	copy_html: 'src/templates/index.html',
-    	css: 'src/styles/*.css'
+    	css: 'src/**/*.css'
     },
     watch: {
     	watch_html: "*.html",
     	watch_css: "styles.css"
     },
-    clean: ['build','src/styles/styles.css']
+    clean: ['build']
 };
-
-
-gulp.task('serve', function () {
-
-    // Serve files from the root of this project
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-
-    gulp.watch(watch_html).on("change", reload);
-    gulp.watch(watch_css).on("change", reload);
-});
-
  
 // Очистка папок и файлов
 gulp.task('clean', function() {
@@ -47,20 +42,49 @@ gulp.task('clean', function() {
         .pipe(clean());
 });
 
-gulp.task('html:copy', function() {
-    gulp.src(path.src.copy_html)
-        .pipe(copy())
-        .pipe(gulp.dest(path.build.html));
+gulp.task('html', function() {
+    return gulp.src(path.html.pages)
+    .pipe(handlebars({}, {
+    	ignorePartials: true,
+    	batch: path.html.componentsPath
+    }))
+    .pipe(rename({
+        dirname: ".",
+        extname: ".html"
+    }))
+    .pipe(gulp.dest(path.build.html));
 });
 
-gulp.task('css:concat', function() {
-    return gulp.src(path.src.css)
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest(path.build.css));
-}); 
+gulp.task('css', function() {
+    return gulp.src(path.css)
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest(path.build.css));
+});
 
-gulp.task('default', [
-    'clean',
-    'html:copy',
-    'css:concat'
-]);
+gulp.task('images', function() {
+    return gulp.src(path.images)
+    .pipe(rename({
+     dirname: "."
+     }))
+     .pipe(gulp.dest(path.build.images));
+});
+
+gulp.task('prod', ['html', 'css', 'images']);
+
+gulp.task('watch', function() {
+   gulp.watch(path.html.pages,['html']);
+   gulp.watch(path.html.components,['html']);
+   gulp.watch(path.css,['css']);
+   gulp.watch(path.images,['images']);
+});
+
+gulp.task('hotReload', function () {
+    browserSync.init({
+     server: {
+      baseDir: path.build.html
+     }
+    });
+    gulp.watch(path.build.root, browserSync.reload);  
+});
+
+gulp.task('default', ['html', 'css', 'images','watch', 'hotReload']);
